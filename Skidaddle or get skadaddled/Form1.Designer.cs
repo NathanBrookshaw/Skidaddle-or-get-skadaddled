aa33e04a08607251e629cfe845ef8f793fd6b724
Nathan Brookshaw <nbrookshaw@SCSS.onmicrosoft.com>
2018-11-05 14:34:43 -0500

    Add project files.

---
+++ Skidaddle or get skadaddled/Form1.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace Skidaddle_or_get_skadaddled
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.blueLabel = new System.Windows.Forms.Label();
+            this.redLabel = new System.Windows.Forms.Label();
+            this.outputLabel = new System.Windows.Forms.Label();
+            this.greenLabel = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // blueLabel
+            // 
+            this.blueLabel.AutoSize = true;
+            this.blueLabel.Location = new System.Drawing.Point(160, 378);
+            this.blueLabel.Name = "blueLabel";
+            this.blueLabel.Size = new System.Drawing.Size(70, 25);
+            this.blueLabel.TabIndex = 0;
+            this.blueLabel.Text = "label1";
+            // 
+            // redLabel
+            // 
+            this.redLabel.AutoSize = true;
+            this.redLabel.Location = new System.Drawing.Point(160, 353);
+            this.redLabel.Name = "redLabel";
+            this.redLabel.Size = new System.Drawing.Size(70, 25);
+            this.redLabel.TabIndex = 1;
+            this.redLabel.Text = "label2";
+            // 
+            // outputLabel
+            // 
+            this.outputLabel.AutoSize = true;
+            this.outputLabel.Location = new System.Drawing.Point(315, 42);
+            this.outputLabel.Name = "outputLabel";
+            this.outputLabel.Size = new System.Drawing.Size(70, 25);
+            this.outputLabel.TabIndex = 2;
+            this.outputLabel.Text = "label1";
+            // 
+            // greenLabel
+            // 
+            this.greenLabel.AutoSize = true;
+            this.greenLabel.Location = new System.Drawing.Point(160, 413);
+            this.greenLabel.Name = "greenLabel";
+            this.greenLabel.Size = new System.Drawing.Size(120, 25);
+            this.greenLabel.TabIndex = 3;
+            this.greenLabel.Text = "greenLabel";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(-3, 413);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(107, 25);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "space bar";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(16, 388);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(26, 25);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "B";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 363);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(30, 25);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "M";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(239, 195);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(176, 78);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_2);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.greenLabel);
+            this.Controls.Add(this.outputLabel);
+            this.Controls.Add(this.redLabel);
+            this.Controls.Add(this.blueLabel);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label blueLabel;
+        private System.Windows.Forms.Label redLabel;
+        private System.Windows.Forms.Label outputLabel;
+        private System.Windows.Forms.Label greenLabel;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Skidaddle or get skadaddled/Form1.cs
@@ -0,0 +1,391 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Skidaddle_or_get_skadaddled
+{
+    public partial class Form1 : Form
+    {
+        int sceneOneChance;
+        int sceneNineChance;
+        int sceneBlankChance;
+        int scene;
+        int state = 0;
+        public Form1()
+        {
+            InitializeComponent();
+            Random randGen = new Random();
+            outputLabel.Text = "you wake up one morning in and you cell door is wide open";
+            redLabel.Text = "do you leave?";
+            blueLabel.Text = "do you stay?";
+            button1.Visible = false;
+
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            Random randGen = new Random();
+            if (e.KeyCode == Keys.M) // red button
+            {
+                if (scene == 0)
+                {
+                    scene = 2;
+                }
+                else if (scene == 2)
+                {
+                    sceneOneChance = randGen.Next(1, 101);
+                     if (sceneOneChance > 70)
+                    {
+                        scene = 5;
+                    }
+                    else
+                    {
+                        scene = 4;
+                    }
+                    
+                }
+                else if (scene == 4)
+                {
+                    scene = 6;
+                }
+                else if (scene == 6)
+                {
+                    scene = 10;
+                }
+                else if (scene == 10)
+                {
+                    sceneOneChance = randGen.Next(1, 101);
+                    if (sceneOneChance > 90)
+                    {
+                        scene = 20;
+                    }
+                    else
+                    {
+                        scene = 19;
+                    } 
+                }
+                else if (scene == 7)
+                {
+                    scene = 8;
+                }
+                else if (scene == 8)
+                {
+                    scene = 16;
+                }
+                else if (scene == 9)
+                {
+                    scene = 12;
+                }
+                else if (scene == 17)
+                {
+                    scene = 24;
+                }
+                else if (scene == 13)
+                {
+                    scene = 21;
+                }
+                else if (scene == 1)
+                {
+                    scene = 0;
+                }
+                else if (scene == 3)
+                {
+                    scene = 0;
+                }
+                else if (scene == 5)
+                {
+                    scene = 0;
+                }
+                else if (scene == 11)
+                {
+                    scene = 0;
+                }
+                else if (scene == 12)
+                {
+                    scene = 0;
+                }
+                else if (scene == 15)
+                {
+                    scene = 0;
+                }
+                else if (scene == 16)
+                {
+                    scene = 0;
+                }
+                else if (scene == 22)
+                {
+                    scene = 0;
+                }
+                else if (scene == 19)
+                {
+                    scene = 0;
+                }
+            }
+
+
+            else if (e.KeyCode == Keys.B)  //blue button press
+            {
+               if (scene == 0)
+                {
+                    scene = 1;
+                }
+
+                else if (scene == 2)
+                {
+                    scene = 3;
+                }
+                else if (scene == 4)
+                {
+                    scene = 7;
+                }
+                else if (scene == 7)
+                {
+                    scene = 9;
+                }
+                else if (scene == 9)
+                {
+                    sceneOneChance = randGen.Next(1, 101);
+                    if (sceneOneChance > 85)
+                    {
+                        scene = 15;
+                    }
+                    else
+                    {
+                        scene = 14;
+                    }
+                }
+                else if (scene == 13)
+                {
+                    scene = 22;
+                }
+                else if (scene == 4)
+                {
+                    scene = 7;
+                }
+                else if (scene == 6)
+                {
+                    scene = 11;
+                }
+                else if (scene == 10)
+                {
+                    scene = 18;
+                }
+                else if (scene == 8)
+                {
+                    scene = 17;
+                }
+                else if (scene == 17)
+                {
+                    scene = 23;
+                }
+            }
+               
+            
+            else if (e.KeyCode == Keys.Space)// green button
+            {
+                if (scene == 9)
+                {
+                    scene = 13;
+                }
+            }
+                
+            
+            /// Display text and game options to screen based on the current scene
+            switch (scene)
+            {
+                // start scene
+                case 0:
+                    outputLabel.Text = "you wake up one morning in and you cell door is wide open";
+                    redLabel.Text = "do you leave?";
+                    blueLabel.Text = "do you stay?";
+                    break;
+                case 1:
+                    outputLabel.Text = "if you stay you will have to stay in prison and you get stabbed two months later.";
+                    redLabel.Text = "you lose press m to restart";
+                    blueLabel.Text = "";
+                    greenLabel.Text = "";
+                    break;
+                case 2:
+                    outputLabel.Text = "you leave you cell and walk down you hallway and when you reach the end you see a gaurd.";
+                    redLabel.Text = "do you kill him?";
+                    blueLabel.Text = "do you try to hide?";
+                    greenLabel.Text = "";
+                    break;
+                case 3:
+                    outputLabel.Text = "You hide the guard finds you and beats you to death and no one ask any questions";
+                    redLabel.Text = "You lose press M to restart";
+                    blueLabel.Text = "";
+                    greenLabel.Text = "";
+                    break;
+                case 4:
+                    outputLabel.Text = " You kill the gaurd and come to another decision.";
+                    redLabel.Text = "Do you steal his uniform and all his gear?";
+                    blueLabel.Text = "Do you steal just his weapons?";
+                    greenLabel.Text = "";
+                    break;
+                case 5:
+                    outputLabel.Text = "You get turned on by the guard and he beats you to death without asking any questions.";
+                    redLabel.Text = "You lose press M to restart";
+                    blueLabel.Text = "";
+                    greenLabel.Text = "";
+                    break;
+                case 6:
+                    outputLabel.Text = "You just take all his gear and then you come to a end of the hall do you turn";
+
+                    redLabel.Text = "Left?";
+                    blueLabel.Text = "Right?";
+                    greenLabel.Text = "";
+                    break;
+                case 7:
+                    outputLabel.Text = "You just take get one grenade and his baton.you then start to make you way down a new hall way ";
+                    redLabel.Text = "Do you turn left?";
+                    blueLabel.Text = " Do you turn right?";
+                    greenLabel.Text = "";
+                    break;
+                case 8:
+                    outputLabel.Text = "You find a single gaurd";
+                    redLabel.Text = "do you beat him?";
+                    blueLabel.Text = "do you taser him?";
+                    greenLabel.Text = "";
+                    break;
+                case 9:
+                    outputLabel.Text = "you find 5 gaurds walking towards you";
+                    redLabel.Text = "?";
+                    blueLabel.Text = "?";
+                    greenLabel.Text = "";
+                    break;
+                case 10:
+                    outputLabel.Text = "you find a guard";
+                    redLabel.Text = " do you disguise yourself as a gaurd?";
+                    blueLabel.Text = "do you run and drop kick him?";
+                    greenLabel.Text = "";
+                    break;
+                case 11:
+                    outputLabel.Text = "you find 5 guards and get knocked out and put back in your cell they realized it was you";
+                    redLabel.Text = "you lose press m to restart";
+                    blueLabel.Text = "";
+                    greenLabel.Text = "";
+                    break;
+                case 12:
+                    outputLabel.Text = "you try to fight them you get knocked out and sent back to your cell";
+                    redLabel.Text = "you lose press m to restart";
+                    blueLabel.Text = "";
+                    greenLabel.Text = "";
+                    break;
+                case 13:
+                    outputLabel.Text = "you run away and hide under a stair case ";
+                    redLabel.Text = "do you stealth kill the gaurds?";
+                    blueLabel.Text = "do you try to wait out the gaurds so they will leave?";
+                    greenLabel.Text = "";
+                    break;
+                case 14:
+                    outputLabel.Text = "you kill all the guards and walk out of the front doors safely";
+                    redLabel.Text = "?";
+                    blueLabel.Text = "?";
+                    greenLabel.Text = "";
+                    break;
+                case 15:
+                    outputLabel.Text = "you for get to throw the grenade and get blown up all over the room and the gaurd have a  big mess to clean up";
+                    redLabel.Text = "you lose press m to restart";
+                    blueLabel.Text = "";
+                    greenLabel.Text = "";
+                    break;
+                case 16:
+                    outputLabel.Text = " you try to beat him he turns on you and you get beat to death";
+                    redLabel.Text = "you lose press m to restart";
+                    blueLabel.Text = "";
+                    greenLabel.Text = "";
+                    break;
+                case 17:
+                    outputLabel.Text = "you taser him you get him on the ground but he hits a button for back up";
+                    redLabel.Text = "do you make a break for it out the door?";
+                    blueLabel.Text = "do you steal the gaurd outfit and walk out the door?";
+                    greenLabel.Text = "";
+                    break;
+                case 18:
+                    outputLabel.Text = "you drop kick the guard and knock him unconscious. then you walk out the door and escape";
+                    redLabel.Text = "?";
+                    blueLabel.Text = "?";
+                    greenLabel.Text = "";
+                    break;
+                case 19:
+                    outputLabel.Text = "you get caught and he takes you back to your cell";
+                    redLabel.Text = "you lose press m to restart";
+                    blueLabel.Text = "?";
+                    greenLabel.Text = "";
+                    break;
+                case 20:
+                    outputLabel.Text = "you escape the prison successfully through the back door";
+                    redLabel.Text = "";
+                    blueLabel.Text = "";
+                    greenLabel.Text = "";
+                    break;
+                case 21:
+                    outputLabel.Text = "you a make an awesome escape";
+                    redLabel.Text = "";
+                    blueLabel.Text = "";
+                    greenLabel.Text = "";
+                    break;
+                case 22:
+                    outputLabel.Text = "you wait and you starve to death";
+                    redLabel.Text = "You lose press M to restart";
+                    blueLabel.Text = "";
+                    greenLabel.Text = "";
+                    break;
+                case 23:
+                    outputLabel.Text = "You walk out the with ear the tower thinks you are a guard looking for the escaping prisoner and you get free";
+                    redLabel.Text = "press any m to end?";
+                    blueLabel.Text = "";
+                    greenLabel.Text = "";
+                    break;
+                case 24:
+                    outputLabel.Text = "if you make a break for it the guard tower in on alert and they snipe you before you can escape";
+                    redLabel.Text = "you lose press m to restart";
+                    blueLabel.Text = "?";
+                    greenLabel.Text = "";
+                    break;
+                case 25:
+                    outputLabel.Text = "you win congratulations ";
+                    redLabel.Text = "";
+                    blueLabel.Text = "";
+                    greenLabel.Text = "";
+                    button1.Visible = true;
+                    break;
+               
+
+
+
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            scene = 0;
+            sceneOneChance = 0;
+            sceneNineChance = 0;
+            sceneBlankChance = 0;
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            scene = 0;
+            sceneOneChance = 0;
+            sceneNineChance = 0;
+            sceneBlankChance = 0;
+        }
+
+        private void button1_Click_2(object sender, EventArgs e)
+        {
+            scene = 0;
+            sceneOneChance = 0;
+            sceneNineChance = 0;
+            sceneBlankChance = 0;
+        }
+    }
+}
